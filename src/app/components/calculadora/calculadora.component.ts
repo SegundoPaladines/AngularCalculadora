2994cbc3638252f27c8714810c546f505917e9e7
SegundoPaladines <segundopaladines@gmail.com>
2023-09-27 23:59:04 -0500

    Operaciones basicas: Suma, Resta, Multiplicación y Divición

--- src/app/components/calculadora/calculadora.component.ts
+++ src/app/components/calculadora/calculadora.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./calculadora.component.css']
 })
 export class CalculadoraComponent {
+  operandoA: number | undefined;
+  operandoB: number | undefined;
 
+  resultado: number | undefined;
+
+  sumar(){
+    if((this.operandoA !== undefined)&&(this.operandoB !== undefined)){
+      this.resultado = this.operandoA + this.operandoB;
+    }
+  }
+  restar(){
+    if((this.operandoA !== undefined)&&(this.operandoB !== undefined)){
+      this.resultado = this.operandoA - this.operandoB;
+    }
+  }
+  multiplicar(){
+    if((this.operandoA !== undefined)&&(this.operandoB !== undefined)){
+      this.resultado = this.operandoA * this.operandoB;
+    }
+  }
+  dividir(){
+    if((this.operandoA !== undefined)&&(this.operandoB !== undefined)){
+      if(this.operandoB !== 0){
+        this.resultado = this.operandoA / this.operandoB;
+      }
+    }
+  }
 }
